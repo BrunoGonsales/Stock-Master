65440f567bd86c816e87fd70be24b6742ca38c6b
BrunoGonsales <cogsbr@outlook.com>
2025-12-27 08:00:42 -0300

    Chore: Initial project structure from spring initializer

---
+++ src/test/java/com/demo/StockMaster/StockMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.StockMaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
