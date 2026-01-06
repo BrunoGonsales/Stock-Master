af6535b090b9e17ecf59ff5a96ba2ee393c62a4d
BrunoGonsales <cogsbr@outlook.com>
2026-01-06 19:20:22 -0300

    Correcting Spring Test name

--- src/test/java/com/brunogonsales/InventoryFlow/InventoryFlowApplicationTests.java
+++ src/test/java/com/brunogonsales/InventoryFlow/InventoryFlowApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class StockMasterApplicationTests {
+class InventoryFlowApplicationTests {
 
 	@Test
 	void contextLoads() {
